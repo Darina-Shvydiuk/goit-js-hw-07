dfe3d47ba921aa6c2a85af982bfc9ac10a71cc43
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-08-04 20:02:37 +0300

    add: first and two task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-console.log(galleryItems);
-console.log(basicLightbox);
+const listGallary = document.querySelector(".gallery");
+
+function createElements() {
+  const markup = galleryItems.reduce((acc, elem) => {
+    return (acc += `<div class="gallery__item">
+  <a class="gallery__link" href="${elem.original}">
+    <img
+      class="gallery__image"
+      src=${elem.preview}
+      data-source="${elem.original}"
+      alt="${elem.description}"
+    />
+  </a>
+</div>`);
+  }, "");
+
+  listGallary.insertAdjacentHTML("beforeend", markup);
+}
+createElements();
+
+listGallary.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const UrlBigImg = event.target.dataset.source;
+  const modal = basicLightbox.create(
+    `
+     <img src=${UrlBigImg} width="800" height="600">
+`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", keyboardEsc);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", keyboardEsc);
+      },
+    }
+  );
+
+  modal.show();
+  function keyboardEsc(event) {
+    if (event.code === "Escape") {
+      modal.close();
+    }
+  }
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,5 +1,22 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-console.log(galleryItems);
-console.log(SimpleLightbox);
+const listGallary = document.querySelector(".gallery");
+
+function createElements() {
+  const markup = galleryItems.reduce((acc, elem) => {
+    return (acc += `<li><a class="gallery__item" href="${elem.original}">
+  <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" />
+</a></li>`);
+  }, "");
+
+  listGallary.insertAdjacentHTML("beforeend", markup);
+}
+createElements();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: "250",
+});
+lightbox();
